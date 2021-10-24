16111cf01fbda8256104a996939d114b00e99042
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-10-24 01:14:18 -0700

    add description to migrations

---
+++ Factory/Migrations/20211024063856_Description.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class Description : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Machines",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Engineers",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Engineers");
+        }
+    }
+}
