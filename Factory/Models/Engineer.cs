2fe9db3dfbc26e32f7148b49a67c390aab4f6dff
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-10-24 00:36:36 -0700

    add description to model

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -9,6 +9,7 @@ namespace Factory.Models
       this.JoinEntities = new HashSet<EngineerMachine>();
     }
     public string Name {get; set;}
+    public string Description {get; set;}
     public int EngineerId{get; set;}
     public virtual ICollection<EngineerMachine> JoinEntities {get; set;}
   }
